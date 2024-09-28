32a7b18e68b6e84dba22673bfaca7d6b5066a7dd
Zias van Nes <vanneszias@icloud.com>
2024-09-28 20:52:27 +0200

    added a dynamic route handler

---
+++ opdracht1/app/api/tests/[id]/route.tsx
@@ -0,0 +1,6 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const id = params.id;
+    return NextResponse.json({ message: `ID is ${id}` });
+}
