275069637ccddf689317ad54890da2d3e58275ae
Zias van Nes <vanneszias@icloud.com>
2024-10-02 12:35:29 +0200

    Refactor book list component to fetch and display author and genre information

--- opdracht1/app/components/list/book.tsx
+++ opdracht1/app/components/list/book.tsx
@@ -6,9 +6,22 @@ interface Book {
   authorId: number;
   genreId: number;
 }
+interface Author {
+  id: number;
+  firstName: string;
+  lastName: number;
+  birthYear: number;
+}
+interface Genre {
+  id: number;
+  name: string;
+}
 
 const BooksList: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [author, setAuthors] = useState<Author[]>([]);
+  const [genre, setGenres] = useState<Genre[]>([]);
+
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -24,8 +37,32 @@ const BooksList: React.FC = () => {
         setLoading(false);
       }
     };
-
     fetchBooks();
+
+    // Fetch authors and genres
+    const fetchAuthors = async () => {
+      try {
+        const response = await fetch("/api/list/authors");
+        const data = await response.json();
+        setAuthors(data);
+      } catch (error) {
+        console.error("Error fetching authors:", error);
+        setAuthors([]);
+      }
+    };
+    fetchAuthors();
+
+    const fetchGenres = async () => {
+      try {
+        const response = await fetch("/api/list/genres");
+        const data = await response.json();
+        setGenres(data);
+      } catch (error) {
+        console.error("Error fetching genres:", error);
+        setGenres([]);
+      }
+    };
+    fetchGenres();
   }, []);
 
   if (loading) {
@@ -34,13 +71,17 @@ const BooksList: React.FC = () => {
   return (
     // First check if there are books to display
     // If there are books, map over the books and display each book
+    // Use the author and genre id to find the author and genre in the author and genre tables
     <div>
       {books.length > 0 ? (
         books.map((book) => (
           <div key={book.id} className="flex justify-between">
             <h3 className="text-xl">{book.title}</h3>
-            <p>{book.authorId}</p>
-            <p>{book.genreId}</p>
+            <p>
+              {author.find((a) => a.id === book.authorId)?.firstName}{" "}
+              {author.find((a) => a.id === book.authorId)?.lastName}
+            </p>
+            <p>{genre.find((g) => g.id === book.genreId)?.name}</p>
           </div>
         ))
       ) : (
