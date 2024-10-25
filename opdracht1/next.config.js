f6dd3e2738378f2b330d19c9169460d469d0b355
Zias van Nes <vanneszias@icloud.com>
2024-10-25 11:22:15 +0200

    .

--- opdracht1/next.config.js
+++ opdracht1/next.config.js
@@ -8,7 +8,7 @@ module.exports = {
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          { key: "Content-Security-Policy", value: "default-src 'none'; connect-src '*'; " }
+          { key: "Content-Security-Policy", value: "default-src '*'; connect-src '*'; " }
         ]
       }
     ];
