ffa114114bcf5b6fab316e483123dea0af002eb3
Zias van Nes <vanneszias@icloud.com>
2024-10-25 11:01:31 +0200

    added nextconfig for crs issues

---
+++ opdracht1/next.config.ts
@@ -0,0 +1,34 @@
+import { NextConfig } from "next";
+
+const config: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'none'; connect-src 'self' https://wm.ziasserver.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default config;
